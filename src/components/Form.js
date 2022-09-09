28fa0551f69d024bb17a96e60a9d89a6458c6413
Tasreen Mowreen <tmowreen97@gmail.com>
2022-09-09 13:28:42 -0400

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
-
+import react, { useState } from "react";
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [number, setNumber] = useState(0);
+  const [isInvalidNumber, setIsInvalidNumber] = useState(null);
+
+  function handleNumberChange(event) {
+    const newNumber = parseInt(event.target.value);
+    if (newNumber >= 0 && newNumber <= 5) {
+      setNumber(newNumber);
+      setIsInvalidNumber(null);
+    } else {
+      setIsInvalidNumber(`${newNumber} is not a valid number!`);
+    }
+  }
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
-      <button type="submit">Submit</button>
+      <input type="number" value={number} onChange={handleNumberChange} />
+      {isInvalidNumber ? <span style={{ color: "red" }}>{isInvalidNumber}</span> : null}
     </form>
   );
 }
---
+++ src/components/ParentComponent.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Form from "./Form";
+import DisplayData from "./DisplayData";
+
+function ParentComponent() {
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Henry");
+
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+    console.log (event.target.value)
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+    console.log (event.target.value)
+  }
+
+  return (
+    <div>
+      <Form
+        firstName={firstName}
+        lastName={lastName}
+        handleFirstNameChange={handleFirstNameChange}
+        handleLastNameChange={handleLastNameChange}
+      />
+      <DisplayData firstName={firstName} lastName={lastName} />
+
+    </div>
+    
+  );
+}
+
+export default ParentComponent;
